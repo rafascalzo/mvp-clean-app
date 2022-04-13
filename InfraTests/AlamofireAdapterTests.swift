0647db198a3dacd867e464bc1b688eb1aef3a919
Rafael Scalzo <rscalzomoraes@hotmail.com>
2022-04-13 18:19:24 -0300

    Move sut creation to a factory helper method

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -43,10 +43,7 @@ class AlamofireAdapterTests: XCTestCase {
     
     func test_post_should_make_request_with_empty_data() {
         let url = makeUrl()
-        let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [UrlProtocolStub.self]
-        let session = Session(configuration: configuration)
-        let sut = AlamofireAdapter(session: session)
+        let sut = makeSut()
         sut.post(to: url, with: nil)
         let exp = expectation(description: "waiting")
         UrlProtocolStub.observerRequest { request in
@@ -54,6 +51,16 @@ class AlamofireAdapterTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
+    }
+}
+
+extension AlamofireAdapterTests {
+    
+    func makeSut() -> AlamofireAdapter {
+        let configuration = URLSessionConfiguration.default
+        configuration.protocolClasses = [UrlProtocolStub.self]
+        let session = Session(configuration: configuration)
+        return AlamofireAdapter(session: session)
     }
 }
 
