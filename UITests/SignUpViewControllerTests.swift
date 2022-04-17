cb4f817030f367b3695886854cf4795f8bbed5f1
Rafael Scalzo <rscalzomoraes@hotmail.com>
2022-04-16 22:07:20 -0300

    Added UI layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewControllerTests.swift
+//  SignUpViewControllerTests
+//
+//  Created by Rafael Scalzo on 16/04/22.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func testExample() {
+    }
+}
