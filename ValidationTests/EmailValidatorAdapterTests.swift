c536e3da31270d12da977c4c21995391e7690cf0
Rafael Scalzo <rscalzomoraes@hotmail.com>
2022-04-18 22:19:47 -0300

    Add Validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,14 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  EmailValidatorAdapterTests
+//
+//  Created by Rafael Scalzo on 18/04/22.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_() {
+    }
+}
