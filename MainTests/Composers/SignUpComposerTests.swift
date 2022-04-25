9277cc2dd3510ca80e5657996c64120910eb1cf1
Rafael Scalzo <rscalzomoraes@hotmail.com>
2022-04-24 23:52:32 -0300

    Rename SignUp Tests

--- MainTests/Composers/SignUpComposerTests.swift
+++ MainTests/Composers/SignUpComposerTests.swift
@@ -1,6 +1,6 @@
 //
-//  SignUpIntegrationTests.swift
-//  SignUpIntegrationTests
+//  SignUpComposerTests.swift
+//  SignUpComposerTests
 //
 //  Created by Rafael Scalzo on 19/04/22.
 //
@@ -8,7 +8,7 @@
 import XCTest
 import Main
 
-class SignUpIntegrationTests: XCTestCase {
+class SignUpComposerTests: XCTestCase {
 
     func test_ui_presentation_integration()  {
         let sut = SignUpComposer.composeController(with: CreateAccountSpy())
